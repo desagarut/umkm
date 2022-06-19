dbba0d3f0d21beb371b3c9b5862a222ac51bc834
Desa Garut <desagarut.net@gmail.com>
2022-06-19 16:51:26 +0700

    Update Toko Profuk Show
    Update Toko Profuk Show
    Fix error link whatsapp

--- themes/umkm/partials/toko_warga/toko_produk_show.php
+++ themes/umkm/partials/toko_warga/toko_produk_show.php
@@ -21,7 +21,7 @@
         <!-- Start Single Product -->
         <div class="single-product">
           <div class="product-image"> <img src="<?= AmbilGaleri($album['gambar'],'kecil') ?>" alt="<?= $album['nama'] ?>">
-            <div class="button"> <a class="button btn btn-success" href="https://wa.me/+62<?= $data['no_hp_toko'] ?>?text=Assalamu'alaikum%2C%20Saya%20tertarik%20dengan%20produk%20yang%20ditawarkan%20di%20website%20*<?= ucfirst($this->setting->sebutan_desa).' '.ucwords($desa['nama_desa']) ?>*.%20Apakah%20<?= $data['nama'] ?>%20masih%20buka%3F%20<?= site_url('first/produk_show/'.$data['id']) ?>" target="_blank" title="Hubungi via whatsapp"> <i class="lni lni-whatsapp"></i> Hubungi </a> </div>
+            <div class="button"> <a class="button btn btn-success" href="https://wa.me/+62<?= $sub['no_hp_toko'] ?>?text=Assalamu'alaikum%2C%20Saya%20tertarik%20dengan%20produk%20yang%20ditawarkan%20di%20website%20*<?= ucfirst($this->setting->sebutan_desa).' '.ucwords($desa['nama_desa']) ?>*.%20Apakah%20<?= $data['nama'] ?>%20masih%20buka%3F%20<?= site_url('first/produk_show/'.$data['id']) ?>" target="_blank" title="Hubungi via whatsapp"> <i class="lni lni-whatsapp"></i> Hubungi </a> </div>
           </div>
           <div class="product-info">
             <h4 class="title"> <a href="<?= site_url('first/produk_show/'.$data['id']) ?>">
